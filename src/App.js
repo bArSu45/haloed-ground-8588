ec47c9d35df767d882fb52ee941a6a45ecbee9e1
unknown <ravindra111223@gmail.com>
2022-11-10 18:21:42 +0530

    adding payment page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import CartPage from './Pages/CartPage/CartPage';
 import HomePage from './Pages/HomePage/HomePage';
 import LoginSignup from './Pages/LoginSignup';
 import Login from './Pages/Login'
+import Payment from './Pages/payment/Payment';
 
 function App() {
   return (
     <>
-      <CartPage/>
+      {/* <CartPage/> */}
       {/* <HomePage/>
       <LoginSignup/>
       <Login/> */}
+      <Payment/>
+
     </>
   );
 }
