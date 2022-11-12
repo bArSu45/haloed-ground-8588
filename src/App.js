84a46a7d8b4da166c34837f4d2fc2d05effc04a0
sayeedanwar648 <sayeedanwar648@gmail.com>
2022-11-12 16:25:19 +0530

    footer added on homepage

--- src/App.js
+++ src/App.js
@@ -17,18 +17,4 @@ export default App
 
 
 
-import React from 'react'
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
 
- const App = () => {
-  return (
-    <div>
-        {/* <Navbar/> */}
-        <Footer/>
-
-    
-    </div>
-  )
-}
-export default App
