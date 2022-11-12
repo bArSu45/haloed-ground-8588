24acc24a0401f7d2852b399f0b3d021c3023866b
sayeedanwar648 <sayeedanwar648@gmail.com>
2022-11-12 07:58:39 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 
 // import Router from 'json-server/lib/server/router';
 
-import './App.css';
+// import './App.css';
 
 
 
+// import React from 'react'
+// import Navbar from './Components/Navbar';
+// import Footer from './Components/Footer';
+
+//  const App = () => {
+//   return (
+//     <div>
+//         {/* <Navbar/> */}
+//         <Footer/>
+
+    
+//     </div>
+//   )
+// }
+// export default App
